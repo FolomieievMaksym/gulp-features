e4a1bc782e0122f9ad62289be2cad8b737a5a55a
Folomieiev Maksym <Folomieiev.m@gmail.com>
2022-08-15 13:29:54 +0300

    add spoiler

--- app/js/script.js
+++ app/js/script.js
@@ -1,38 +1,70 @@
 "use strict";
-const burger = document.querySelector('.burger'),
-	menu = document.querySelector('.menu'),
-	header = document.querySelector('.header'),
-	body = document.querySelector('body');
+const burger = document.querySelector(".burger"),
+   header = document.querySelector(".header"),
+   body = document.querySelector("body");
 
-window.addEventListener('load', () => {
-	if (burger) {
+window.addEventListener("load", () => {
+   function qs(element) {
+      let newEl = document.querySelector(element);
+      if (newEl) return newEl;
+   }
+   function qa(element) {
+      let newEl = document.querySelectorAll(element);
+      if (newEl) return newEl;
+   }
 
-		body.addEventListener('click', burgerToggle)
-		function burgerToggle(e) {
-			// alert("Click") // Для проверки вызова функции кликом
-			if (e.target.closest('.burger')) {
-				burger.classList.toggle('active');
-				menu.classList.toggle('active');
-				header.classList.toggle('active');
-				body.classList.toggle('lock');
-				window.addEventListener('scroll', closeBurger) // Закрывает бургер при скролле в том случае, когда для Body не задан класс 'lock'
-			} else if (!e.target.closest('.burger')) {
-				burger.classList.remove('active');
-				menu.classList.remove('active');
-				header.classList.remove('active');
-				body.classList.remove('lock');
-				window.removeEventListener('scroll', closeBurger)
-			}
-		}
+   // ! Burger
+   if (burger) {
+      body.addEventListener("click", burgerToggle);
+      function burgerToggle(e) {
+         if (e.target.closest(".burger")) {
+            if (burger.classList.contains("active")) {
+               burger.classList.remove("active");
+               header.classList.remove("active");
+               body.classList.remove("lock");
+            } else {
+               burger.classList.add("active");
+               header.classList.add("active");
+               body.classList.add("lock");
+               window.addEventListener("scroll", closeBurger); // Закрывает бургер при скролле в том случае, когда для Body не задан класс 'lock'
+            }
+         } else if (!e.target.closest(".burger") && !e.target.closest(".menu")) {
+            burger.classList.remove("active");
+            header.classList.remove("active");
+            body.classList.remove("lock");
+            window.removeEventListener("scroll", closeBurger);
+         }
+      }
+      function closeBurger() {
+         //Необязательная дополнительная проверка
+         if (burger.classList.contains("active")) {
+            burger.classList.remove("active");
+            menu.classList.remove("active");
+            header.classList.remove("active");
+            body.classList.remove("lock");
+            window.removeEventListener("scroll", closeBurger);
+         }
+      }
+   }
 
-		function closeBurger() {
-			if (burger.classList.contains('active')) { //Необязательная дополнительная проверка
-				burger.classList.remove('active');
-				menu.classList.remove('active');
-				header.classList.remove('active');
-				body.classList.remove('lock');
-				window.removeEventListener('scroll', closeBurger)
-			}
-		}
-	}
-})
+   // ! Spoiler.html
+   if (qs(".spoiler")) {
+      body.addEventListener("click", switchCard);
+
+      function switchCard(e) {
+         const spoiler = qs(".spoiler"),
+            spoilerBody = qs(".spoiler__wrapper");
+
+         if (e.target.closest(".spoiler__preview")) {
+            spoiler.classList.toggle("opened");
+            if (!spoiler.classList.contains("opened")) {
+               spoilerBody.style.height = null;
+            } else {
+               console.log("opened");
+               let spoilerWrapperScrollHeight = spoilerBody.scrollHeight;
+               spoilerBody.style.height = spoilerWrapperScrollHeight + "px";
+            }
+         }
+      }
+   }
+});
